a93ddf7c6bc6eaee6017570dc9762b9f50a05ccb
rus733 <9282716617@mail.ru>
2022-02-03 16:44:58 +0300

    fix: remove old code in timer.js

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,9 +1,4 @@
 const timer = (deadline) => {
-  //
-  // const daysVal = document.querySelector('.count_1 > span');
-  // const hoursVal = document.querySelector('.count_2 > span');
-  // const minutesVal = document.querySelector('.count_3 > span');
-  // const secondesVal = document.querySelector('.count_4 > span');
   const daysVal = document.querySelectorAll('.count_1');
   const hoursVal = document.querySelectorAll('.count_2');
   const minutesVal = document.querySelectorAll('.count_3');
@@ -13,8 +8,6 @@ const timer = (deadline) => {
   const hoursForms = [' Час ', ' часа ', ' Часов '];
   const minutesForms = [' Минута ', ' Минуты ', ' Минут '];
   const secondesForms = [' Секунда ', ' Секунды ', ' Секунд '];
-  const date = new Date();
-  //
 
   const declOfNum = (n, textForms) => {
     n = Math.abs(n) % 100;
@@ -31,21 +24,13 @@ const timer = (deadline) => {
     return textForms[2];
   };
 
-  // const addZero = (arg) => {
-  //   if (arg < 10) {
-  //     return '0' + arg;
-  //   } else {
-  //     return arg;
-  //   }
-  // };
   const addZero = (arg) => (arg < 10 ? '0' + arg : arg);
 
   // остаток времени
 
   const getTimeRemaining = () => {
-    //const year = date.getFullYear() + 1;
-    const dateStop = new Date(deadline); //.getTime();
-    const dateNow = new Date(); //.getTime();
+    const dateStop = new Date(deadline);
+    const dateNow = new Date();
     const diff = (dateStop - dateNow) / 1000;
     let days = diff > 0 ? Math.floor(diff / 60 / 60 / 24) : 0;
     let hours = diff > 0 ? Math.floor((diff / 60 / 60) % 24) : 0;
@@ -72,12 +57,7 @@ const timer = (deadline) => {
   // вывод данных
 
   const updateClock = () => {
-    //const time = timeFormat();
     const getTime = getTimeRemaining(); //
-    // const days = addZero(getTime.days);
-    // const hours = addZero(getTime.hours);
-    // const minutes = addZero(getTime.minutes);
-    // const secondes = addZero(getTime.secondes);
 
     daysVal.forEach((el) => {
       el.innerHTML = `${declOfNum(getTime.days, dayForms)}:</br> <span>${addZero(getTime.days)}</span>`;
